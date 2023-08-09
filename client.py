122431d47f4ad55652ddd2a7d2a5d18023b20a67
Akihiro Nagai <77012577+akihironagai@users.noreply.github.com>
2023-08-09 19:12:19 +0900

    Update

--- client.py
+++ client.py
@@ -1,19 +1,63 @@
 import socket
 import ssl
 import os
+import threading
+import queue
 
-hostname = '127.0.0.1'
-context = ssl.create_default_context(
-    cafile=os.getenv('SSL_CA_CERT_FILE'),
-)
+import config
 
-with socket.create_connection((hostname, 8443)) as sock:
-    with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-        ssock.do_handshake()
-        ssock.setblocking(False)
+client_proxy_vpn_side_packet: queue.Queue[bytes] = queue.Queue()
+client_proxy_proxy_side_packet: queue.Queue[bytes] = queue.Queue()
+
+
+def client_proxy_proxy_side():
+    print("Client Proxy Proxy Side")
+    address = config.REMOTE_PROXY_ADDRESS
+    context = ssl.create_default_context(
+        cafile=os.getenv("SSL_CA_CERT_FILE"),
+    )
+
+    with socket.create_connection(address) as sock:
+        with context.wrap_socket(sock, server_hostname=address[0]) as ssock:
+            ssock.do_handshake(True)
+            ssock.setblocking(False)
+            while True:
+                try:
+                    data = ssock.recv(2048)
+                except ssl.SSLWantReadError:
+                    data = None
+                if data:
+                    print(f"[C] Remote Proxy -> Local Proxy: {data}")
+                    client_proxy_proxy_side_packet.put(data)
+                if not client_proxy_vpn_side_packet.empty():
+                    data = client_proxy_vpn_side_packet.get()
+                    # TODO: 正しく送信できた場合のみ Queue から削除する
+                    # VPN -> Proxy
+                    ssock.sendall(client_proxy_vpn_side_packet.get())
+
+
+def client_proxy_vpn_side():
+    print("Client Proxy VPN Side")
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.bind(config.LOCAL_PROXY_ADDRESS)
+        #  sock.setblocking(False)
         while True:
-            ssock.send(b"hello client\n")
             try:
-                print(ssock.recv(1024))
-            except ssl.SSLWantReadError:
-                pass
+                data, addr = sock.recvfrom(2048)
+            except BlockingIOError:
+                data = None
+            if data:
+                print(f"[C] Local VPN -> Local Proxy: {data}")
+                client_proxy_vpn_side_packet.put(data)
+            if not client_proxy_proxy_side_packet.empty():
+                # Proxy -> VPN
+                sock.sendto(
+                    client_proxy_proxy_side_packet.get(), config.REMOTE_PROXY_ADDRESS
+                )
+
+
+if __name__ == "__main__":
+    proxy_side_thread = threading.Thread(target=client_proxy_proxy_side)
+    vpn_side_thread = threading.Thread(target=client_proxy_vpn_side)
+    proxy_side_thread.start()
+    vpn_side_thread.start()
--- server.py
+++ server.py
@@ -1,16 +1,18 @@
 import socket
 import ssl
+import time
+
+import config
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain('127.0.0.1.pem', '127.0.0.1-key.pem')
 
-with socket.create_server(('127.0.0.1', 8443)) as sock:
+with socket.create_server(config.REMOTE_PROXY_ADDRESS) as sock:
     sock.listen(5)
     with context.wrap_socket(sock, server_side=True) as ssock:
         conn, addr = ssock.accept()
         conn.setblocking(False)
         while True:
-            conn.send(b"hello server\n")
             try:
                 print(conn.recv(1024))
             except ssl.SSLWantReadError:
