2e21acecbee6c41efd85c625726f85437f604d8e
Akihiro Nagai <77012577+akihironagai@users.noreply.github.com>
2023-08-09 16:05:44 +0900

    Update

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ context = ssl.create_default_context(
 
 with socket.create_connection((hostname, 8443)) as sock:
     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-        print(ssock.version())
+        ssock.do_handshake()
         ssock.setblocking(False)
         while True:
             ssock.send(b"hello client\n")
