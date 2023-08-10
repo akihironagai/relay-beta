d1131ad03598c1e61a0ba5b5d0bf3d383c4ff3a9
Akihiro Nagai <77012577+akihironagai@users.noreply.github.com>
2023-08-10 10:26:52 +0900

    多分動く

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ from_remote_proxy_packet: queue.Queue[bytes] = queue.Queue()
 from_local_vpn_packet: queue.Queue[bytes] = queue.Queue()
 
 def local_proxy_to_remote_proxy():
-    address = config.TCP_TUNNEL
+    address = (config.TCP_TUNNEL_ADDRESS, config.TCP_TUNNEL_PORT)
     context = ssl.create_default_context(
         cafile=os.getenv("SSL_CA_CERT_FILE"),
     )
@@ -18,6 +18,7 @@ def local_proxy_to_remote_proxy():
     with socket.create_connection(address) as sock:
         with context.wrap_socket(sock, server_hostname=address[0]) as ssock:
             ssock.do_handshake(True)
+            print(ssock.version)
             ssock.setblocking(False)
             while True:
                 try:
@@ -25,11 +26,11 @@ def local_proxy_to_remote_proxy():
                 except ssl.SSLWantReadError:
                     data = None
                 if data:
-                    print(f"[C] Remote Proxy -> *Local Proxy: {data}")
+                    print(f"[C] Remote Proxy -> *Local Proxy: {len(data)}")
                     from_remote_proxy_packet.put(data)
                 if not from_local_vpn_packet.empty():
                     local_data = from_local_vpn_packet.get()
-                    print(f"[C] *Local Proxy -> Remote Proxy: {local_data}")
+                    print(f"[C] *Local Proxy -> Remote Proxy: {len(local_data)}")
                     ssock.sendall(local_data)
 
 
@@ -38,7 +39,7 @@ def local_vpn_to_local_proxy():
         sock.bind(config.LOCAL_PROXY_ADDRESS)
         while True:
             data, _ = sock.recvfrom(2048)
-            print(f"[C] Local VPN -> *Local Proxy: {data}")
+            print(f"[C] Local VPN -> *Local Proxy: {len(data)}")
             from_local_vpn_packet.put(data)
 
 
@@ -49,11 +50,36 @@ def local_proxy_to_local_vpn():
                 data = from_remote_proxy_packet.get()
             except queue.Empty:
                 continue
-            print(f"[C] *Local Proxy -> Local VPN: {data}")
+            print(f"[C] *Local Proxy -> Local VPN: {len(data)}")
             sock.sendto(data, config.LOCAL_VPN_ADDRESS)
+
+
+def local_vpn_and_local_proxy():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.bind(config.LOCAL_PROXY_ADDRESS)
+
+        def recv():
+            while True:
+                data, _ = sock.recvfrom(2048)
+                print(f"[C] Local VPN -> *Local Proxy: {len(data)}")
+                from_local_vpn_packet.put(data)
+
+        def send():
+            while True:
+                try:
+                    data = from_remote_proxy_packet.get()
+                except queue.Empty:
+                    return
+                print(f"[C] *Local Proxy -> Local VPN: {len(data)}")
+                sock.sendto(data, config.LOCAL_VPN_ADDRESS)
+
+        threading.Thread(target=recv).start()
+        threading.Thread(target=send).start()
+
+        while True:
+            ...
 
 
 if __name__ == "__main__":
     threading.Thread(target=local_proxy_to_remote_proxy).start()
-    threading.Thread(target=local_vpn_to_local_proxy).start()
-    threading.Thread(target=local_proxy_to_local_vpn).start()
+    threading.Thread(target=local_vpn_and_local_proxy).start()
--- server.py
+++ server.py
@@ -9,46 +9,48 @@ from_local_proxy_packet: queue.Queue[bytes] = queue.Queue()
 from_remote_vpn_packet: queue.Queue[bytes] = queue.Queue()
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(f'{config.REMOTE_PROXY_ADDRESS[0]}.pem', f'{config.REMOTE_PROXY_ADDRESS[0]}-key.pem')
+context.load_cert_chain(config.CERT_FILE, config.KEY_FILE)
 
 def local_proxy_to_remote_proxy():
-    with socket.create_server(config.TCP_TUNNEL) as sock:
+    with socket.create_server(("0.0.0.0", config.TCP_TUNNEL_PORT)) as sock:
         sock.listen(5)
         with context.wrap_socket(sock, server_side=True) as ssock:
             conn, addr = ssock.accept()
             conn.setblocking(False)
             while True:
-                try:
-                    data = conn.recv(1024)
-                except ssl.SSLWantReadError as e:
-                    data = None
+                try: data = conn.recv(1024)
+                except ssl.SSLWantReadError: data = None
                 if data:
-                    print(f"[S] Local Proxy -> *Remote Proxy: {data}")
+                    print(f"[S] Local Proxy -> *Remote Proxy: {len(data)}")
                     from_local_proxy_packet.put(data)
                 if not from_remote_vpn_packet.empty():
                     remote_data = from_remote_vpn_packet.get()
-                    print(f"[S] *Remote Proxy -> Local Proxy: {remote_data}")
+                    print(f"[S] *Remote Proxy -> Local Proxy: {len(remote_data)}")
                     conn.sendall(remote_data)
 
-def remote_proxy_to_remote_vpn():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        while True:
-            try:
-                data = from_local_proxy_packet.get()
-            except queue.Empty:
-                continue
-            print(f"[S] *Remote Proxy -> Remote VPN: {data}")
-            sock.sendto(data, config.REMOTE_VPN_ADDRESS)
-
-def remote_vpn_to_remote_proxy():
+def remote_vpn_and_remote_proxy():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.bind(config.REMOTE_PROXY_ADDRESS)
+
+        def recv():
+            while True:
+                data, _ = sock.recvfrom(2048)
+                print(f"[S] Remote VPN -> *Remote Proxy: {len(data)}")
+                from_remote_vpn_packet.put(data)
+
+        def send():
+            while True:
+                try: data = from_local_proxy_packet.get()
+                except queue.Empty: return
+                print(f"[S] *Remote Proxy -> Remote VPN: {len(data)}")
+                sock.sendto(data, config.REMOTE_VPN_ADDRESS)
+
+        threading.Thread(target=recv).start()
+        threading.Thread(target=send).start()
+
         while True:
-            data, _ = sock.recvfrom(2048)
-            print(f"[S] Remote VPN -> *Remote Proxy: {data}")
-            from_remote_vpn_packet.put(data)
+            ...
 
 if __name__ == "__main__":
     threading.Thread(target=local_proxy_to_remote_proxy).start()
-    threading.Thread(target=remote_vpn_to_remote_proxy).start()
-    threading.Thread(target=remote_proxy_to_remote_vpn).start()
+    remote_vpn_and_remote_proxy()
